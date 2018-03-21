c07bb2eadd1bbc6df12503488cf871b44fe2432d
Walrus <walrus@hotmail.sg>
2018-03-21 21:21:19 +0800

    UPDATE enemy slow movement (prototype)

--- Assets/Scripts/EnemySlow.cs
+++ Assets/Scripts/EnemySlow.cs
@@ -1,12 +1,34 @@
 ﻿using UnityEngine;
 
 public class EnemySlow : Enemy {
+	#region Variables
+	bool hasDodged = false;
+	float x, y, z;
+	#endregion
+
 	protected override void Move() {
+		// dodge left/right if path in front is tower / range
+		if (hasDodged) {
+			// move forward
+			x = -cubeSize / 2;
+			y = -cubeSize / 2;
+			z = 0;
+			hasDodged = false;
+		}
+		else {
+			// dodge or move forward
+			// if left or right lower weight
+				// dodge
+				hasDodged = true; // if dodged
+			// else
+				// move forward
+		}
+		
 		// move in a random direction
-		bool xAxis = Random.value > 0.5f;
-		float x = xAxis ? (Random.Range(0, 2) * 2 - 1) * cubeSize / 2 : 0;
-		float y = -cubeSize / 2;
-		float z = xAxis ? 0 : (Random.Range(0, 2) * 2 - 1) * cubeSize / 2;
+		//bool xAxis = Random.value > 0.5f;
+		//float x = xAxis ? (Random.Range(0, 2) * 2 - 1) * cubeSize / 2 : 0;
+		//float y = -cubeSize / 2;
+		//float z = xAxis ? 0 : (Random.Range(0, 2) * 2 - 1) * cubeSize / 2;
 		
 		pivot.Translate(x, y, z);
 
